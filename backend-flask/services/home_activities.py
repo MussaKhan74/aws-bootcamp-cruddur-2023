6cb8b98afc2a4c9a7488d612c0276e833c786832
MussaKhan74 <MarkeLoof@gmail.com>
2023-03-10 22:25:04 +0000

    implemented aws cloudwatch logs

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,6 +6,8 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
+  # def run(logger):
+  #   logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
