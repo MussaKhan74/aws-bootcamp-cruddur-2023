71610963fe4842e49f39ba37ab273b626f206f27
MussaKhan74 <MarkeLoof@gmail.com>
2023-03-24 01:11:45 +0000

    added service for cognito token

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -92,8 +93,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -147,9 +148,16 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  # Logging Authorization Access Token
+  
+  # app.logger.debug("AUTH HEADER")
+  # app.logger.debug(
+  #   request.headers.get('Authorization')
+  # )
+
   # FOR CLOUD WATCH
   # data = HomeActivities.run(logger=LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
